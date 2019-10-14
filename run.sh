3fc122dda3f3b1a9180432cd48a23d52568546c8
Max Zurawski <maksymilian.zurawski@consileon.pl>
2019-10-14 14:28:54 +0200

    Waiting with dispatcher to start for services register and sensortypes

--- run.sh
+++ run.sh
@@ -17,6 +17,16 @@ echo "Waiting for the rabbit service to start "
 echo "**************************************************************"
 while ! `nc -z rabbit 15672 `; do sleep 3; done
 
+echo "**************************************************************"
+echo "Waiting for the register service to start "
+echo "**************************************************************"
+while ! `nc -z register 8102 `; do sleep 3; done
+
+echo "**************************************************************"
+echo "Waiting for the sensortypes service to start "
+echo "**************************************************************"
+while ! `nc -z sensortypes 8101 `; do sleep 3; done
+
 echo "#############################################"
 echo "Ready to rumble. Starting dispatcher service"
 echo "#############################################"
