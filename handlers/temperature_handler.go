f2ffa56d38e254f49053f8b52f8f4bfa6ff07c04
Max Zurawski <maksymilian.zurawski@consileon.pl>
2019-10-15 13:30:42 +0200

    Using caches to validate registered sensors

--- handlers/temperature_handler.go
+++ handlers/temperature_handler.go
@@ -6,8 +6,12 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/xdevices/dispatcher/caches/sensors"
+
+	"github.com/google/uuid"
+	"github.com/xdevices/dispatcher/publishers"
+
 	"github.com/labstack/echo"
-	"github.com/labstack/gommon/log"
 	"github.com/xdevices/utilities/stringutils"
 )
 
@@ -20,7 +24,16 @@ func TemperatureHandler(c echo.Context) error {
 
 	if !isUuidValid {
 		msgString := fmt.Sprintf("given uuid: [%s] is not valid uuid", rawUuid)
-		log.Warn(msgString)
+		publishers.Logger().Warn(uuid.New().String(), rawUuid, msgString)
+		return c.JSON(http.StatusBadRequest, errors.New(msgString))
+	}
+
+	// check if sensor is registered
+	registeredSensor := sensors.Cache.GetByUuid(rawUuid)
+	if registeredSensor == nil {
+		msgString := fmt.Sprintf("sensor with uuid: [%s] is not registered", rawUuid)
+		publishers.Logger().Warn(uuid.New().String(), rawUuid, msgString)
+		// otherwise return status bad request
 		return c.JSON(http.StatusBadRequest, errors.New(msgString))
 	}
 
@@ -28,13 +41,13 @@ func TemperatureHandler(c echo.Context) error {
 	tempValue, err := strconv.ParseFloat(rawValue, 64)
 	if err != nil {
 		msgString := fmt.Sprintf("given value: [%s] is not parsable to float number", rawValue)
-		log.Warn(msgString)
+		publishers.Logger().Warn(uuid.New().String(), rawUuid, msgString)
 		return c.JSON(http.StatusBadRequest, errors.New(msgString))
 	} else {
 		value = tempValue
 	}
 
 	successMsg := fmt.Sprintf("measurement received from sensor with uuid: [%s]. temperature was: [%.2f]", rawUuid, value)
-	log.Info(successMsg)
+	publishers.Logger().Info(uuid.New().String(), rawUuid, successMsg)
 	return c.NoContent(http.StatusAccepted)
 }
